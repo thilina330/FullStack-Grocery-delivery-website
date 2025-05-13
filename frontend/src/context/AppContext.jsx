ecaa16f039a42e4f0cb2bb3f71bf491610005079
Thilina Lakshan <126488588+thilina330@users.noreply.github.com>
2025-05-13 14:26:54 +0530

    create app context file

--- frontend/src/context/AppContext.jsx
+++ frontend/src/context/AppContext.jsx
@@ -1,13 +1,23 @@
-import { Children, createContext } from "react";
-
+import { createContext, useContext, useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 
 export const AppContext = createContext();
 
 
 export const AppContextProvider = ({Children}) => {
-    return <AppContext.Provider>
+
+    const navigate = useNavigate();
+    const [user, setUser] = useState(null);
+    const [isSeller, setIsSeller] = useState(false);
+
+    const value = {navigate, user,setUser, setIsSeller,isSeller};
+    
+    return <AppContext.Provider value={value}>
         {Children}
     </AppContext.Provider>
 }
 
+export const useAppContext = () => {
+    return useContext(AppContext)
+}
