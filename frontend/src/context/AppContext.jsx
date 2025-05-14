4e9fae29cfda69c9c0077fbad28259494dc68e44
Thilina Lakshan <126488588+thilina330@users.noreply.github.com>
2025-05-14 21:53:59 +0530

    update App Context

--- frontend/src/context/AppContext.jsx
+++ frontend/src/context/AppContext.jsx
@@ -6,10 +6,10 @@ export const AppContext = createContext();
 export const AppContextProvider = ({children}) => {
 
     const navigate = useNavigate();
-    const [user,setUser] = useState(true);
-    const [isSeller,setisSeller] = useState(false);
+    const [user,setUser] = useState(null);
+    const [isSeller,setIsSeller] = useState(false);
 
-    const value = {navigate, user,setUser,setisSeller,isSeller};
+    const value = {navigate, user,setUser,setIsSeller,isSeller};
 
    return <AppContext.Provider value={value}>
        {children}
