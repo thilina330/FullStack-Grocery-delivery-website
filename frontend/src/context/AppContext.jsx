8af1e6a0b04394509be0eb4d86b4807ca3c7c1c7
Thilina Lakshan <126488588+thilina330@users.noreply.github.com>
2025-05-18 15:52:08 +0530

    add product

--- frontend/src/context/AppContext.jsx
+++ frontend/src/context/AppContext.jsx
@@ -1,5 +1,6 @@
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { dummyProducts } from "../assets/assets";
 
 export const AppContext = createContext();
 
@@ -8,8 +9,19 @@ export const AppContextProvider = ({children}) => {
     const navigate = useNavigate();
     const [user,setUser] = useState(null);
     const [isSeller,setIsSeller] = useState(false);
+    const [showUserLogin, setShowUserLogin] = useState(false);
+    const [products, setProducts] = useState([]);
 
-    const value = {navigate, user,setUser,setIsSeller,isSeller};
+    const fetchProducts = async () => {
+        setProducts(dummyProducts);
+    }
+
+    useEffect(() => {
+        fetchProducts()
+    },[])
+
+
+    const value = {navigate, user,setUser,setIsSeller,isSeller, showUserLogin,setShowUserLogin,products};
 
    return <AppContext.Provider value={value}>
        {children}
