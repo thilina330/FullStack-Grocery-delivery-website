956b9dd06516e922a12d8fef657f15de79d0ad1b
Thilina Lakshan <126488588+thilina330@users.noreply.github.com>
2025-05-18 15:52:25 +0530

    create best seller part

---
+++ frontend/src/components/BestSeller.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import ProductCard from './ProductCard'
+
+const BestSeller = () => {
+  return (
+    <div className='mt-16'>
+      <p className='text-2xl md:text-3xl font-medium'>Best Sellers</p>
+      <div>
+        <ProductCard/>
+      </div>
+    </div>
+  )
+}
+
+export default BestSeller
