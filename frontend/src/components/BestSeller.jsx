9e312ba6d97812bc64d78967e5a7878cef658b2e
Thilina Lakshan <126488588+thilina330@users.noreply.github.com>
2025-07-28 21:59:05 +0530

    update best seller component

--- frontend/src/components/BestSeller.jsx
+++ frontend/src/components/BestSeller.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
 import ProductCard from './ProductCard'
+import { useAppContext } from '../context/AppContext';
 
 const BestSeller = () => {
+  // This component is used to display the best-selling products.
+  // It currently renders a single ProductCard component with an empty product object.
+  
+  const {products} = useAppContext();
+
   return (
     <div className='mt-16'>
       <p className='text-2xl md:text-3xl font-medium'>Best Sellers</p>
       <div>
-        <ProductCard/>
+         <ProductCard product={products[0]} /> 
       </div>
     </div>
   )
